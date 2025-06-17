bc752b5a8ff4e36a74ca907f320be2a55321aa28
brandon <2837515@vikes.csuohio.edu>
2025-06-17 16:06:39 -0500

    Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+
+      <header>
+        <div>
+          <h1>Dondon & Nini's Blog</h1>
+        </div>
+      </header>
+
+      <main>
+
+      </main>
+
+    </BrowserRouter>
+  );
+}
+
+export default App;
