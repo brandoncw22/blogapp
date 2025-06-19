e3a965857cb83a238083e74c869c29c8bdcd479d
Brandon <2837515@vikes.csuohio.edu>
2025-06-19 03:36:22 +0100

    Added sqlite db

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Post from '../components/Post';
+import './styles/Home.css';
+
 function Home() {
-  return <h2>Welcome Home</h2>;
+  const posts = LoadPosts();
+
+  return (
+    <div className="posts-container">
+
+    </div>
+  );
+}
+
+function LoadPosts () {
+  const [posts, setPosts] = useState([]);
+  axios.get("http://localhost:3000/loadPosts")
+    .then(res => setPosts(res.data))
+    .catch(error => console.log(error));
+
+  return posts;
+  
 }
 
 export default Home;
---
+++ server/server.js
@@ -0,0 +1,27 @@
+//Express setup
+const express = require('express');
+const app = express();
+const PORT = 3030;
+
+const sqlite = require('sqlite3').verbose();
+
+//Setting up the db and the tables
+const db = new sqlite.Database('./blogapp.db', (err) =>{
+    if(err) return console.error(err.message);
+    console.log('Connnected to DB')
+});
+
+db.run(`
+    CREATE TABLE IF NOT EXISTS posts (
+        postID INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        c
+    )
+    `);
+
+
+app.get('/loadPosts', (req, res) =>{
+
+});
+
+app.listen(PORT, console.log(`Server running on http://localhost:${PORT}`));
