94c2d16230c90ca7ec71a4e6cfa5c55515452a8a
Brandon <2837515@vikes.csuohio.edu>
2025-06-24 18:03:35 +0100

    Added more login functionality
    - Login component added
    - Login popup added
    - Logout is handled

--- client/src/App.js
+++ client/src/App.js
@@ -7,80 +7,19 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
-import axios from 'axios';
+import Login from './components/Login';
+
 
 function App() {
 
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [isOpen, setIsOpen] = useState(false);
-  const loginFormRef = useRef(null);
-  const tabButtonRef = useRef(null);
 
-  useEffect(() => {
-        if (loginFormRef.current) {
-            if (isOpen) {
-                loginFormRef.current.style.display = 'block';
-                loginFormRef.current.style.height = 'fit-content';
-            } else {
-                loginFormRef.current.style.display = 'none';
-                loginFormRef.current.style.height = '0';
-            }
-        }
-  }, [isOpen]);
-
-  const handlePopup = async (e) => {
-    e.preventDefault();
-
-    setIsOpen(!isOpen);
-
-  }
-  const handleLogin = async (e) => {
-    e.preventDefault();
-
-    try {
-      const res = await axios.post('http://localhost:3030/login', {
-        username,
-        password
-      });
-
-      const success = res.data.success;
-      const user = res.data.username;
-
-      if (success) {
-        setIsLoggedIn(true);
-      } else {
-
-      }
-    } catch (err) {
-      console.error("Login failed:", err.response?.data || err.message);
-    }
-  }
   return (
     <Router>
       <header>
         <div className="admin_bar">
-        <div className="login_popup">
-          <button className="tab_button" onClick={handlePopup} ref={tabButtonRef}>☰</button>
-          <form className="login_form" onSubmit={handleLogin} ref={loginFormRef}>
-            <input 
-              type="text"
-              name="username"
-              placeholder="Username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              />
-              <input 
-              type="password"
-              name="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              />
-              <button type="submit">Login</button>
-            </form>
-        </div>
+          <Login setIsLoggedIn={setIsLoggedIn} isLoggedIn={isLoggedIn}/>
         </div>
         <div id="header-bar" >
           <h1 className="title">Dondon & Nini's Blog</h1>
---
+++ client/src/components/Login.js
@@ -0,0 +1,83 @@
+
+import { useEffect, useState, useRef } from 'react';
+import axios from 'axios';
+import Popup from 'reactjs-popup';
+
+function Login({setIsLoggedIn, isLoggedIn}) {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  if (isLoggedIn) {
+    return(
+      <div className="login_popup">
+        <Popup trigger={<button className="tab_button">⚙</button>}
+        modal nested>
+          {(close) => (
+            <form className="login_form">
+              <button onClick={async (e) =>{
+                  e.preventDefault();
+                  setIsLoggedIn(false);
+                  close();
+              }}>Logout</button>
+            </form>
+          )}
+          
+        </Popup>
+        
+      </div>
+    );
+    }
+    return(
+        <div className="login_popup">
+          <Popup trigger={<button className="tab_button">⚙</button>}
+          modal nested>
+            {(close) => (
+                <form className="login_form">
+              
+                <input 
+                  type="text"
+                  name="username"
+                  placeholder="Username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  />
+                  <input 
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <button onClick={async (e) => {
+                    e.preventDefault();
+
+                    try {
+                    const res = await axios.post('http://localhost:3030/login', {
+                        username,
+                        password
+                    });
+
+                    const success = res.data.success;
+                    const user = res.data.username;
+
+                    if (success) {
+                        setIsLoggedIn(true);
+                        close();
+                    } else {
+
+                    }
+                    } catch (err) {
+                    console.error("Login failed:", err.response?.data || err.message);
+                    }
+                  }}>Login</button>
+                </form>
+              )}
+            
+          </Popup>
+          
+        </div>
+    );
+}
+
+export default Login;
